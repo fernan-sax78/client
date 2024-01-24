a9a0533b775be72ed85e763b1d223721eeeee349
Fernando <fernan.sax@hotmail.com>
2024-01-25 00:09:53 +0100

    latest for now

--- src/components/Navigation/HeaderGroup/headerGroup.styles.js
+++ src/components/Navigation/HeaderGroup/headerGroup.styles.js
@@ -5,7 +5,7 @@ import { StyleSheet } from "react-native";
 export const styles = new StyleSheet.create({
     container : {
         backgroundColor : "#171717",
-        height : 95,
+        height : 120,
     },
     content : {
         flex : 1,
